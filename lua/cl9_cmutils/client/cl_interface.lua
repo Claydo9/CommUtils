56e562877ef7f2452ced27bd83d503337e318b6b
Clayton <claydo9@gmail.com>
2023-04-16 17:13:31 +1000

    Add player list to menu

--- lua/cl9_cmutils/client/cl_interface.lua
+++ lua/cl9_cmutils/client/cl_interface.lua
@@ -1,19 +1,22 @@
 
 cl9_cmutils = {}
 cl9_cmutils.players = player.GetAll()
+cl9_cmutils.playerButtons = {}
+cl9_cmutils.canvas = nil
 local uiScale = ScrH() / 1080
-
 
 function cl9_cmutils.doDerma()
     local canvas 
-    
-    if canvas then
+
+    if cl9_cmutils.canvas then
+        canvas = cl9_cmutils.canvas
         canvas:Show()
         canvas:MakePopup()
         return 
     end
 
     canvas = vgui.Create( "DFrame" )
+    cl9_cmutils.canvas = canvas
     canvas:SetSize( 400 * uiScale, 600 * uiScale )
     canvas:Center()
     canvas:SetTitle( "Comm Utils" )
@@ -28,17 +31,30 @@ function cl9_cmutils.doDerma()
     local playerList = vgui.Create( "DScrollPanel", canvas )
     playerList:SetPos( 5 * uiScale, 30 * uiScale )
     playerList:SetSize( 390 * uiScale , 560 * uiScale )
-    playerList.Paint = function( _, w, h )
-        draw.RoundedBox( 0, 0, 0, w, h, Color( 37, 37, 38, 255 ) )
-    end 
-end
+    local function paintList()
+        playerList.Paint = function( _, w, h )
+            draw.RoundedBox( 0, 0, 0, w, h, Color( 37, 37, 38, 255 ) )     
+            for i, v in SortedPairs( cl9_cmutils.players ) do 
+                cl9_cmutils.playerButtons[i] = vgui.Create( "DButton", playerList )
+                local btn = cl9_cmutils.playerButtons[i]
+                btn:SetPos( 5 * uiScale,  ( ( 42 * uiScale ) * ( i - 1 ) ) + ( 5 * uiScale ) + ( (5 * ( i - 1 ) ) * uiScale ) )
+                btn:SetSize( 380 * uiScale, 42 * uiScale )
+                local avtr = vgui.Create( "AvatarImage", btn )
+                avtr:SetPos( 5 * uiScale, 5 * uiScale )
+                avtr:SetSize( 32, 32 )
+                avtr:SetPlayer( v, 32 )
+                
+                btn.Paint = function( _, bW, bH )
+                    draw.RoundedBox( 0, 0, 0, bW, bH, Color( 51, 51, 51, 255 ) )
+                end 
+            end                   
+        end
+    end
+    paintList()
 
-function cl9_cmutils.updateList()
-    cl9_cmutils.players = player.GetAll()
+    hook.Add( "PlayerInitialSpawn", "cl9_cmutils_playerinitialspawn", paintList )
+    hook.Add( "PlayerDisconnected", "cl9_cmutils_playerdisconnect", paintList )
 end
-
-hook.Add( "PlayerInitialSpawn", "cl9_cmutils_playerinitialspawn", cl9_cmutils.updateList )
-hook.Add( "PlayerDisconnected", "cl9_cmutils_playerdisconnect", cl9_cmutils.updateList )
 
 concommand.Add( "cl9_cmutils", cl9_cmutils.doDerma )
 
